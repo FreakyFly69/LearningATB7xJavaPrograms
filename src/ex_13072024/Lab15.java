80bc6d80248c20d1322683a4627612cb219fe14d
FreakyFly69 <dongrep22@gmail.com>
2024-07-13 12:41:36 +0530

    All files pushed

---
+++ src/ex_13072024/Lab15.java
@@ -0,0 +1,45 @@
+package ex_13072024;
+
+public class Lab15 {
+
+    public static void main(String[] args) {
+
+        //Data Type
+        //Primitive
+        //boolean
+            //True or false
+        boolean is_married = true;
+        boolean are_you_a_bird = false;
+        System.out.println(is_married);
+        System.out.println(are_you_a_bird);
+
+
+        //Numerical
+            //intregal - byte, short, int, long
+
+        byte b = 100;
+        short s = 101;
+        int age =  130;
+        long phone_num = 9876543210L;
+
+            //char - char - A,B
+        char grade = 'A'; // A,B,C,D
+
+        //floating
+        float pi = 3.14f;
+        //double
+        double d = 118.563;
+
+
+        //Non Primitive
+        //String namr -> "pramod"
+        String name = "pramod";
+        String name2= "p";
+        
+
+
+
+
+
+    }
+}
