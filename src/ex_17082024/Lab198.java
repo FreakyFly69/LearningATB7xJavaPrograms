bb77869619e65af5ab53e5fd5035ccdad73e783c
FreakyFly69 <dongrep22@gmail.com>
2024-09-01 10:36:53 +0530

    All files pushed

---
+++ src/ex_17082024/Lab198.java
@@ -0,0 +1,15 @@
+package ex_17082024;
+
+public class Lab198 {
+
+    public static void main(String[] args) {
+
+        String s = "10";
+        Integer a = Integer.valueOf(s);
+        System.out.println(a);
+
+
+    }
+
+
+}
---
+++ src/ex_17082024/Lab199.java
@@ -0,0 +1,25 @@
+package ex_17082024;
+
+public class Lab199 {
+
+    public static void main(String[] args) {
+
+        apiAutomation a = new apiAutomation();
+
+    }
+
+
+}
+
+class apiAutomation{
+
+    static {
+        // write code for mysql
+        System.out.println("Loaded data from mysql");
+    }
+    {
+        System.out.println("Loaded data from csv");
+    }
+
+
+}
